f1628612c71d5afac473b3b5960617e099d92a07
VirtualKley <javolkley@gmail.com>
2021-09-13 22:37:58 -0500

    Subiendo Backend

---
+++ Controllers/TarjetaCreditoController.cs
@@ -0,0 +1,86 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarjetaCreditoController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        public TarjetaCreditoController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get api/TarjetaCredito
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TarjetaCredito>>> GetTarjetaCredito()
+        {
+            return await _context.TarjetaCredito.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        //Get api/TarjetaCredito/id
+        public  async Task<ActionResult<TarjetaCredito>> GetTarjetaCredito(int id)
+        {
+            var tarjetaCredito = await _context.TarjetaCredito.FindAsync(id);
+            if (tarjetaCredito == null) return NotFound();
+            return tarjetaCredito;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTarjetaCredito(int id, TarjetaCredito tarjetaCredito)
+        {
+            if (id != tarjetaCredito.Id) return BadRequest();
+            _context.Entry(tarjetaCredito).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TarjetaCreditoExits(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TarjetaCredito>> PostTarjetaCredito(TarjetaCredito tarjetaCredito)
+        {
+            _context.TarjetaCredito.Add(tarjetaCredito);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTarjetaCredito", new { id = tarjetaCredito.Id }, tarjetaCredito);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TarjetaCredito>> DeleteTarjetaCredito(int id)
+        {
+            var tarjetaCredito = await _context.TarjetaCredito.FindAsync(id);
+            if (tarjetaCredito == null) return NotFound();
+            _context.TarjetaCredito.Remove(tarjetaCredito);
+            await _context.SaveChangesAsync();
+            return tarjetaCredito;
+        }
+
+        private bool TarjetaCreditoExits(int id)
+        {
+            return _context.TarjetaCredito.Any(e => e.Id == id);
+        }
+    }
+}
